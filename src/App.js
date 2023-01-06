8a758bece4ec3fe2cd85b738f5fc37111d4ffe6a
Chaitanya Ratta <chaitanyaratta07@gmail.com>
2023-01-06 23:37:13 +0530

    initial files

--- src/App.js
+++ src/App.js
@@ -3,21 +3,71 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App p-5">
+    <h2>Purchase Order</h2>
+    <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">S.No</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="S.NO"/>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Item</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Item"/>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Item Name</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Item Name"/>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">UOM</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="UOM"/>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">HSN CODE</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="HSN CODE"/>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Quantity</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Quantity"/>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Rate</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Rate"/>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Gross Value</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Gross Value"/>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">CGST(%)</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="CGST(%)"/>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">CGST</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="CGST"/>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">SGST(%)</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="SGST(%)"/>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">IGST(%)</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="IGST(%)"/>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">IGST</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="IGST"/>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">NetValue</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="NetValue"/>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Remarks</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Remarks"/>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
     </div>
   );
 }
